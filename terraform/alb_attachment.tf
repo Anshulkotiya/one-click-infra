ea02fada6eedb77ce869fb8f0d57b79da8226f70
Anshul Kotiya <anshul_kotiya@Anshul-Kotiya-pc.localdomain>
2026-07-11 17:21:42 +0530

    d

--- terraform/alb_attachment.tf
+++ terraform/alb_attachment.tf
@@ -6,3 +6,10 @@ resource "aws_lb_target_group_attachment" "kibana" {
 
   port = var.kibana_port
 }
+
+
+resource "aws_lb_target_group_attachment" "elasticsearch" {
+  target_group_arn = aws_lb_target_group.kibana_tg.arn
+  target_id        = aws_instance.elasticsearch.id
+  port             = var.kibana_port
+}
